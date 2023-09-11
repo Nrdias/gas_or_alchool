661d7ffac4725035169ca627e6447bfb8f0e1487
Dias <98723703+Nrdias@users.noreply.github.com>
2023-09-11 09:12:40 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:entrada_dados/GasOrAlchool.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+      MaterialApp(
+        home: GasOrAlchool(),
+        debugShowCheckedModeBanner: false,
+      )
+  );
+}
